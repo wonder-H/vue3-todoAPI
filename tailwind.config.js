b85429b7502ee3d94fc6f26cdb69ff6737240e88
wonder-H <soimar@naver.com>
2022-06-03 23:49:13 +0900

    Feat: install tailwind, postcss, autoprefixer to apply css style

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+};
